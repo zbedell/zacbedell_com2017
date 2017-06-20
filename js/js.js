c7336bf5741a4e96bb925feb1730c899b61c97df
Zachary Bedell <zbedell@bpcs.com>
2017-06-20 09:13:57 -0700

    incremental progress

--- js/js.js
+++ js/js.js
@@ -2,18 +2,12 @@ var updateClock = function(){
 
   var currentDate = new Date();
   var epochTime = currentDate.getTime();
-  console.log('Unix Epoch Time = ' + epochTime);
-  $('#millisecond-display').html(epochTime);
-
-  var humanTime = moment(currentDate.toISOString()).format('h:mm:ss A');
-
-  $('#datetime-display').html(humanTime);
+  $('#epoch-display').html(epochTime);
 
   var mmt = moment();
   var bday = moment('1978-12-19 16:30:00');
   var age = mmt.diff(bday, 'seconds');
-  console.log('Seconds Since Zac: '+ age);
-  $('#diffSeconds-output').html(age);
+  $('#ssz').html(age);
 
   setTimeout(updateClock, 1000);
 
